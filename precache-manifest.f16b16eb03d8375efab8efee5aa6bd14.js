0a7fbab4b9f6caf300edda127966a942f2f5d7ff
Justin Schnurer <JSchnurer@slb.com>
2021-02-09 12:09:09 -0500

    Updates

---
+++ precache-manifest.f16b16eb03d8375efab8efee5aa6bd14.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3f294a726f95b14dcdec",
+    "url": "/chimp-test-react/static/css/main.a4c1ded7.chunk.css"
+  },
+  {
+    "revision": "3f294a726f95b14dcdec",
+    "url": "/chimp-test-react/static/js/main.3f294a72.chunk.js"
+  },
+  {
+    "revision": "4d4949a9a9f903a5cc4a",
+    "url": "/chimp-test-react/static/js/1.4d4949a9.chunk.js"
+  },
+  {
+    "revision": "ca2dd2b2b9f67f0cc5e5",
+    "url": "/chimp-test-react/static/js/runtime~main.ca2dd2b2.js"
+  },
+  {
+    "revision": "15bb604aa973411efa3900d7c9d7b4b4",
+    "url": "/chimp-test-react/index.html"
+  }
+];
